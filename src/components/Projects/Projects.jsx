ae72906598500d62a5764f7428b52b0a8009559f
a52cents <kalimlouardi@gmail.com>
2023-11-14 10:51:25 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/About/About.css 	modified:   src/components/Projects/Projects.css 	modified:   src/components/Projects/Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,10 +2,31 @@ import React from 'react'
 import './Projects.css'
 
 const Projects = () => {
+  const cards = [
+    {
+      title: "CRUD/API",
+      
+    }]
   return (
-    <div >
-   
+    <section id="project">
+      <div className="wrapper">
+        <h1>Mes Projets</h1>
+        <h2></h2>
+        <h3>Simple CRUD/API website</h3>
+        <div className="content-container">
+          
+          {cards.map((card, index) => (
+            <div key={index} className="card-wrapper">
+              <div className="card">
+                <div className={"content-card-front"}>
+                  <p>{card.title}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+    </section>
   )
 }
 
